d3c995748705d14a1f793f16dba29f18a5fa9441
Rasmus Clausen <vez64ryj@gmail.com>
2019-11-24 15:26:52 +0100

    Added project

---
+++ Assets/Art/Test Scene/DotProductTest.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DotProductTest : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        float dot = Vector3.Dot(new Vector3(-1,0,0),new Vector3(-1,0,0));
+        Debug.Log(dot);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Art/Test Scene/LightTurning.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightTurning : MonoBehaviour
+{
+    [SerializeField]
+    float speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 rotation = new Vector3(-speed*Time.deltaTime,0,0);
+        transform.Rotate(rotation,Space.Self);  
+    }
+}
